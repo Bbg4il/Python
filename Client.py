c1e7842f7e9b0e37e461dadfb460ab9c433ab8bb
Bbg4il <89661948+Bbg4il@users.noreply.github.com>
2022-07-14 08:37:45 -0600

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,58 @@
+"""
+ClientScript
+
+This file establishes a connection to the server and transmits information.
+Appends information to a file created on the server.
+Terminate connection once information is received and input into file.
+
+Abigail Sauco
+000860402
+07.07.22
+
+"""
+
+#import modules
+import CronTab
+import socket
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 4455
+FORMAT = "utf-8"
+SIZE = 1024
+def main():
+    
+#Staring a TCP socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Connecting to the server
+client.connect(IP, PORT)
+
+#Opening and reading the file data
+file = open("data/yt.txt", "r")
+data = file.read()
+
+#Sending the filename to the server
+client.send("yt.txt".encode(FORMAT))
+msg = client.recv(SIZE).decode(FORMAT)
+
+print(f"[SERVER]: {msg}")
+
+#Sending the file data to the server
+client.send(data.encode(FORMAT))
+msg = client.recv(SIZE).decode(FORMAT)
+print(f"[SERVER]: {msg}")
+
+#close file
+file.close()
+
+#close connection
+client.close()
+if __name__ == "__main__":
+    
+main()
+
+#Put try except for error handling. If sysadmin.py does not exist.
+
+
+
+
---
+++ CronTabJob.py
@@ -0,0 +1,39 @@
+"""
+CronTab Job Maker
+
+This script regularly runs a cron job at boot.
+
+-SysAdminTask.py creates a NEW file with all information
+-A Crontab job ran the Client.py script and sent the NEW file over to server
+-The NEW file is opened on the server machine to prove that all content exists in that file
+
+Abigail Sauco
+000860402
+07.07.22
+
+Skeleton code for crontab provided by Marcel Toszer in CPRG 261 Module 8.
+
+Run (pip3 install python-crontab) on linux machine to install crontab.
+
+"""
+
+#import crontab from crontab (pip3 install python-crontab)
+from crontab import CronTab
+
+userName = 'abigail2'
+cron = CronTab(user=userName)
+
+# create new cron job & define parameters for the job
+job = cron.new(command='/usr/bin/python /home/abigail2/Documents/CPRG-261/Project_3/SysAdminTask.py >> /home/abigail2/Documents/CPRG-261/Project_3/Cron3.log')
+#job.minute.every(1) RUN AT STARTUP
+
+cron.write()
+
+# create new cron job to run Client.py
+job = cron.new(command='/usr/bin/python /home/abigail2/Documents/CPRG-261/Project_3/Client.py')
+job.minute.every(1)
+
+# Clearing the restrictions of a job
+job.clear()
+
+#
